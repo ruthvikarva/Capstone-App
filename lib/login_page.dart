a8d13a2aeed63693db99732405c52b1df8acd170
Ruthvik Arvapally <ruthvikarva@.noreply.github.com>
2020-04-01 23:38:26 -0400

    Almost done with login/register

---
+++ lib/login_page.dart
@@ -0,0 +1,132 @@
+import 'dart:math';
+
+import 'package:flutter/material.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+
+class LoginPage extends StatefulWidget {
+
+  @override
+  State<StatefulWidget> createState() => new _LoginPageState();
+}
+
+enum FormType{
+  login,
+  register
+}
+
+class _LoginPageState extends State<LoginPage> {
+
+  final formKey = new GlobalKey<FormState>();
+
+  String _email;
+  String _password;
+  FormType _formType = FormType.login;
+
+  bool validateSafe(){
+    final form = formKey.currentState;
+    if(form.validate()){
+      form.save();
+      return true;
+    }
+    else{
+      return false;
+    }
+}
+
+void validateAndSubmit() async{
+    if(validateSafe()) {
+      try {
+        if(_formType == FormType.login) {
+        FirebaseUser user = (await FirebaseAuth.instance
+            .signInWithEmailAndPassword(
+            email: _email, password: _password)) as FirebaseUser;
+        print('Signed in: ${user.uid}');
+      }
+        else {
+          FirebaseUser user = (await FirebaseAuth.instance.createUserWithEmailAndPassword(email: _email, password: _password)).user;
+          print('Registered user: ${user.uid}');
+        }
+      }
+      catch (e) {
+        print('Error: $e');
+      }
+    }
+  }
+
+  void moveToRegister(){
+    formKey.currentState.reset();
+    setState(() {
+      _formType = FormType.register;
+    });
+  }
+
+  void moveToLogin(){
+    formKey.currentState.reset();
+    setState(() {
+      _formType = FormType.login;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return new Scaffold(
+      appBar: new AppBar(
+        title: new Text('Capstone App'),
+      ),
+      body: new Container(
+        padding: EdgeInsets.all(16.0),
+        child: new Form(
+          key: formKey,
+          child: new Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: buildInputs() + buildSubmitButtons()
+          ),
+        )
+      )
+    );
+  }
+
+  List<Widget> buildInputs(){
+    return [new TextFormField(
+      decoration: new InputDecoration(labelText: 'Email'),
+      validator: (value) => value.isEmpty ? 'Email can\'t be empty' : null,
+      onSaved: (value) => _email = value,
+    ),
+      new TextFormField(
+        decoration: new InputDecoration(labelText: 'Password'),
+        obscureText: true,
+        validator: (value) => value.isEmpty ? 'Password can\'t be empty' : null,
+        onSaved: (value) => _password = value,
+      ),
+    ];
+  }
+
+  List<Widget> buildSubmitButtons(){
+    if(_formType == FormType.login){
+      return [
+        new RaisedButton(
+          child: new Text('Login', style: new TextStyle(fontSize: 20.0)),
+          onPressed: validateAndSubmit,
+        ),
+        new FlatButton(
+          child: new Text('Create an account', style: new TextStyle(fontSize: 20.0)),
+          onPressed: moveToRegister,
+        )
+      ];
+    }
+    else{
+      return [
+        new RaisedButton(
+          child: new Text('Create an Account', style: new TextStyle(fontSize: 20.0)),
+          onPressed: validateAndSubmit,
+        ),
+        new FlatButton(
+          child: new Text('Have an account? Login', style: new TextStyle(fontSize: 20.0)),
+          onPressed: moveToLogin,
+        )
+      ];
+    }
+  }
+}
+
+
