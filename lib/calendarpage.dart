a7926952980870e34e5a99126e7c9ea2763164f9
Steven D <stevendavi123@gmail.com>
2020-04-09 13:48:37 -0400

    Very basic calendar implementation Subject to change

--- lib/calendarpage.dart
+++ lib/calendarpage.dart
@@ -1,24 +1,90 @@
-// calendarpage.dart
-
 import 'package:flutter/material.dart';
 import 'package:table_calendar/table_calendar.dart';
 
+
 class CalendarPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Meal Calendar',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: MealCalendarPage(),
+    );
+  }
+}
+class MealCalendarPage extends StatefulWidget {
+  @override
+  _MealCalendarPageState createState() => _MealCalendarPageState();
+}
+
+class _MealCalendarPageState extends State<MealCalendarPage> {
+  CalendarController _controller;
+
+  @override
+  void initState() {
+    super.initState();
+    _controller = CalendarController();
+  }
 
   @override
   Widget build(BuildContext context) {
-    CalendarController _controller;
-
     return Scaffold(
       appBar: AppBar(
         title: Text('Meal Calendar'),
       ),
-      body: Center(
-        child: RaisedButton(
-            child: Text('Back To HomeScreen'),
-            color: Theme.of(context).primaryColor,
-            textColor: Colors.white,
-            onPressed: () => Navigator.pop(context)
+      body: SingleChildScrollView(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+            TableCalendar(
+              initialCalendarFormat: CalendarFormat.week,
+              calendarStyle: CalendarStyle(
+                  todayColor: Colors.orange,
+                  selectedColor: Theme.of(context).primaryColor,
+                  todayStyle: TextStyle(
+                      fontWeight: FontWeight.bold,
+                      fontSize: 18.0,
+                      color: Colors.white)),
+              headerStyle: HeaderStyle(
+                centerHeaderTitle: true,
+                formatButtonDecoration: BoxDecoration(
+                  color: Colors.orange,
+                  borderRadius: BorderRadius.circular(20.0),
+                ),
+                formatButtonTextStyle: TextStyle(color: Colors.white),
+                formatButtonShowsNext: false,
+              ),
+              startingDayOfWeek: StartingDayOfWeek.monday,
+              onDaySelected: (date, events) {
+                print(date.toIso8601String());
+              },
+              builders: CalendarBuilders(
+                selectedDayBuilder: (context, date, events) => Container(
+                    margin: const EdgeInsets.all(4.0),
+                    alignment: Alignment.center,
+                    decoration: BoxDecoration(
+                        color: Theme.of(context).primaryColor,
+                        borderRadius: BorderRadius.circular(10.0)),
+                    child: Text(
+                      date.day.toString(),
+                      style: TextStyle(color: Colors.white),
+                    )),
+                todayDayBuilder: (context, date, events) => Container(
+                    margin: const EdgeInsets.all(4.0),
+                    alignment: Alignment.center,
+                    decoration: BoxDecoration(
+                        color: Colors.orange,
+                        borderRadius: BorderRadius.circular(10.0)),
+                    child: Text(
+                      date.day.toString(),
+                      style: TextStyle(color: Colors.white),
+                    )),
+              ),
+              calendarController: _controller,
+            )
+          ],
         ),
       ),
     );
