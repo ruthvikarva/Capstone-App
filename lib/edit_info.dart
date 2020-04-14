646e13d57323a2b199251d3dbd141efb9a880dfe
Purnima824 <Purnima824@gmail.com>
2020-04-14 01:41:21 -0400

    added a form for the editting information page

--- lib/edit_info.dart
+++ lib/edit_info.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_form_builder/flutter_form_builder.dart';
+
 
 class EditInfo extends StatefulWidget {
   @override
@@ -6,18 +8,119 @@ class EditInfo extends StatefulWidget {
 }
 
 class _EditInfoState extends State<EditInfo> {
-  String _name;
+  bool validate=true;
+  final GlobalKey<FormBuilderState> _formKey= GlobalKey<FormBuilderState>();
+
+  Widget _buildName(){
+    return FormBuilderTextField(
+      attribute: 'name',
+      initialValue: "Name", //Name of the user obtained when signing up
+      validators: [FormBuilderValidators.required()],
+      decoration: InputDecoration(
+        labelText: "Name",
+      ),
+    );
+  }
+
+  Widget _buildCalories(){
+    return FormBuilderTextField(
+      attribute: 'calories',
+      initialValue: "2000", //Name of the user obtained when signing up
+      validators: [
+        FormBuilderValidators.min(800),
+        FormBuilderValidators.max(3500),
+        FormBuilderValidators.numeric()
+      ],
+      decoration: InputDecoration(
+        labelText: "Calorie Goal",
+      ),
+    );
+  }
+
+
+  Widget _buildDiet(){
+    return FormBuilderCheckboxList(
+      decoration: InputDecoration(
+          labelText: "Dietary Preferences"
+      ),
+      attribute: "diets",
+      initialValue: ["None"],
+      options: [
+        FormBuilderFieldOption(value: "None"),
+        FormBuilderFieldOption(value: "Vegan"),
+        FormBuilderFieldOption(value: "Vegetarian")
+      ],
+    );
+  }
+
+  Widget _buildAllergies(){
+    return FormBuilderCheckboxList(
+      decoration: InputDecoration(
+          labelText: "Allergies"
+      ),
+      attribute: "allergies",
+      initialValue: ["None"],
+      options: [
+        FormBuilderFieldOption(value: "None"),
+        FormBuilderFieldOption(value: "Eggs"),
+        FormBuilderFieldOption(value: "Milk"),
+        FormBuilderFieldOption(value: "Peanuts"),
+        FormBuilderFieldOption(value: "Seafood"),
+        FormBuilderFieldOption(value: "Soy"),
+        FormBuilderFieldOption(value: "Tree Nuts"),
+        FormBuilderFieldOption(value: "Wheat"),
+      ],
+    );
+  }
+
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text("Editing Information"),
-        backgroundColor: Colors.redAccent,
       ),
-      body: Container(
-        margin: EdgeInsets.all(24),
+
+      body: ListView(
+        children: <Widget>[
+          Padding(
+            padding: EdgeInsets.all(24),
+            child: Column(
+              children: <Widget>[
+                FormBuilder(
+                  key: _formKey,
+                  autovalidate: true,
+                  child:Column(
+                    children: <Widget>[
+                      _buildName(),
+                      _buildCalories(),
+                      _buildDiet(),
+                      _buildAllergies()
+                    ],
+                  ),
+
+                ),
+                SizedBox(height: 100,),
+                RaisedButton(
+                  child: Text("Submit"),
+                  onPressed: () {
+                    if(_formKey.currentState.saveAndValidate()){
+                      _formKey.currentState.value;
+                    }
+                  },
+                ),
+
+              ],
+            ),
+          ),
+        ],
       ),
+
+
     );
   }
 }
+
+
+
+
