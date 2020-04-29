673e69b4a6f3b28bb01b1344e568f4b2327518bf
sdboom <63025653+sdboom@users.noreply.github.com>
2020-04-29 15:36:16 -0400

    Revert "Lots of changes to calendar stuff."
    This reverts commit b3bba88dd2a3723ff531c996192e50ced10ca85f.

--- lib/firestoreEvent.dart
+++ lib/firestoreEvent.dart
@@ -2,5 +2,3 @@ import 'package:firebase_helpers/firebase_helpers.dart';
 import 'eventModel.dart';
 
 DatabaseService<EventModel> eventDBS = DatabaseService<EventModel>("events",fromDS: (id,data) => EventModel.fromDS(id, data), toMap:(event) => event.toMap());
-
-//test
